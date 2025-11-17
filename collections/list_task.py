c15873227dab22c9aa0c54e0d12d86616a8376b8
Aakashdoraisamy <aakashdorai03@gmail.com>
2025-11-17 20:13:30 +0530

    Added code for without built in method

--- collections/list_task.py
+++ collections/list_task.py
@@ -26,7 +26,7 @@ for i in range(len(l)):
 print(f'After removing {l1} the final list is:',l)
 
 
-# WITHOUT INDEX
+# WITHOUT USING INDEX METHOD
 
 l = [10,20,30,40,50,60]
 l1 = int(input('enter Your input to check the index:'))
@@ -38,11 +38,74 @@ for i in range(len(l)):
 print(f'index of {l1} in list is {index} index place')
         
         
-# WITHOUT REVERSE 
+# WITHOUT USING REVERSE METHOD
 
 l = [100,'Python','SQL','HTML','CSS',100]
 new = []
 for i in range(len(l)-1, -1, -1): # i = 5,4,3,2,1,0 it accesses the list in reverse order # taken input from last index value 5 and then stop until reach -1 because we need until zero and then to reverese we use -1 because deafult we use [::-1] for reverse
     new += [l[i]]
 print(new)
+
+# WITHOUT USING APPEND METHOD
+
+l = [1, 2, 3, 4]
+x = int(input("Enter element to append: "))
+l += [x] 
+print(l)
+
+# WITHOUT USING EXTEND METHOD
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+for i in l2:
+    l1 += [i]  
+print(l1)
+
+# WITHOUT USING INSERT METHOD
+
+l = [10, 20, 30, 40]
+p = int(input("Enter position to insert: "))
+val = int(input("Enter value to insert: "))
+l = l[:p] + [val] + l[p:]
+print(l)
+
+# WITHOUT USING COPY METHOD
+
+l = [1, 2, 3, 4]
+l_copy = [i for i in l]  
+print(l_copy)
+
+# WITHOUT USING CLEAR METHOD
+
+l = [1, 2, 3, 4]
+while len(l) > 0:
+    del l[0]
+print(l)
+
+# WITHOUT USING SORT METHOD
+
+l = [5, 1, 4, 2, 3]
+for i in range(len(l)):
+    for j in range(i+1, len(l)):
+        if l[i] > l[j]:
+            l[i], l[j] = l[j], l[i]
+print(l)
+
+# WITHOUT USING MAX METHOD
+
+l = [5, 1, 7, 3]
+max_val = l[0]
+for i in l:
+    if i > max_val:
+        max_val = i
+print(max_val)
+
+# WITHOUT USING MIN METHOD
+
+l = [5, 1, 7, 3]
+min_val = l[0]
+for i in l:
+    if i < min_val:
+        min_val = i
+print(min_val)
 
