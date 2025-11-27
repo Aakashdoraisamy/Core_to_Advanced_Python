28612eef872e42491f623b81d9ee079abe37a6dd
Aakashdoraisamy <aakashdorai03@gmail.com>
2025-11-27 21:01:19 +0530

    Added 3rd_largest_num

--- python_programs/array/second_largest_num.py
+++ python_programs/array/second_largest_num.py
@@ -12,7 +12,29 @@ for num in arr:
 print("Second largest:", second)
 
 
+# arr = [10, 20, 4, 45, 99, 99]
+
+# unique_sorted = sorted(set(arr), reverse=True)
+# print("Second largest:", unique_sorted[1])
+
+
 arr = [10, 20, 4, 45, 99, 99]
 
-unique_sorted = sorted(set(arr), reverse=True)
-print("Second largest:", unique_sorted[1])
+first = second = third = float('-inf')
+
+for num in arr:
+    if num > first:
+        third = second
+        second = first
+        first = num
+    elif num > second and num != first:
+        third = second
+        second = num
+    elif num > third and num != first and num != second:
+        third = num
+
+if third == float('-inf'):
+    print("No 3rd largest number found")
+else:
+    print("Third largest:", third)
+
