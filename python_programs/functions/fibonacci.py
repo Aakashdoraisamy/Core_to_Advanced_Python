905e186ae7495e2919b632298421d568066913a3
Aakashdoraisamy <aakashdorai03@gmail.com>
2025-11-29 21:46:57 +0530

    Updated fibonacci

--- python_programs/functions/fibonacci.py
+++ python_programs/functions/fibonacci.py
@@ -6,3 +6,13 @@ def fibonacci(n):
         a,b = b,a+b
     return l
 print(fibonacci(int(input('Enter a number:'))))
+
+
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter number of terms: "))
+fibonacci(n)
